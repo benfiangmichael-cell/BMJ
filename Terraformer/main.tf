2b111b9d02d115e88929b855acd35d5bf3df11c3
BENFIANG Michael <benfiangmichael@gmail.com>
2025-12-01 10:17:46 +0100

    Delete Terraformer directory

--- Terraformer/main.tf
+++
@@ -1,7 +0,0 @@
-provider "null" {}
-
-resource "null_resource" "create_users" {
-  provisioner "local-exec" {
-    command = "bash ../1-Script-Bash/create_users.sh"
-  }
-}
--- Terraformer/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "server_ip" {
-  type = string
-}
-
-variable "ssh_port" {
-  type    = number
-  default = 2222
-}
